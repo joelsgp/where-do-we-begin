879f5a96c4d9b654c505e281ec62a5d93a945bee
jmcb <joelsgp@protonmail.com>
2023-06-24 23:29:10 +0100

    Fix ffmpeg merger

--- merge-test.sh
+++ merge-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eux
 
-ffmpeg -i The_Podcast_1_-_The_Future_Of_Podcasting_Fixed_.mp3 -i Where_do_we_begin_ep1.jpg -tune stillimage -c:a copy test.mp4 -y
+ffmpeg -i The_Podcast_1_-_The_Future_Of_Podcasting_Fixed_.mp3 -loop 1 -i Where_do_we_begin_ep1.jpg -tune stillimage -c:a copy -shortest test.mp4 -y
