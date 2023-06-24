c5f373f1cdf7706ff1d45f2c9aab21491f922279
jmcb <joelsgp@protonmail.com>
2023-06-24 23:33:31 +0100

    ffmpeg merge: low framerate for faster converting

--- merge-test.sh
+++ merge-test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -eux
 
-ffmpeg -i The_Podcast_1_-_The_Future_Of_Podcasting_Fixed_.mp3 -loop 1 -i Where_do_we_begin_ep1.jpg -tune stillimage -c:a copy -shortest test.mp4 -y
+ffmpeg \
+    -i The_Podcast_1_-_The_Future_Of_Podcasting_Fixed_.mp3 \
+    -framerate 1/10 -loop 1 -i Where_do_we_begin_ep1.jpg \
+    -tune stillimage -c:a copy -shortest \
+    test.mp4 \
+    -y
