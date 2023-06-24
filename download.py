490b15728c135cb4238bed6442dccd33dd906bda
jmcb <joelsgp@protonmail.com>
2023-06-25 00:00:36 +0100

    Switch from bs4 to podcastparser

--- download.py
+++ download.py
@@ -1,54 +1,20 @@
+#!/usr/bin/env python
+
 from datetime import datetime
 from typing import NamedTuple
 
-import bs4
+import podcastparser
 import requests
-
-
-# todo publish module for easy future users?
-# ok nvm scratch this file and try this instead
-# https://github.com/gpodder/podcastparser
-
 
 URL_RSS = "https://feed.podbean.com/wayneradiotv/feed.xml"
 
 
-class Episode(NamedTuple):
-    title: str
-    description: str
-    link: str
-    timestamp: datetime
-    mp3_url: str
-    jpg_url: str
 
-
-Episodes = list[Episode]
-Items = list[bs4.Tag]
-
-
-def get_items() -> Items:
-    response = requests.get(URL_RSS)
-    soup = bs4.BeautifulSoup(response.content, features="html.parser")
-
-    channel = soup.find("channel")
-    items = channel.find_all("item")
-
-    return items
-
-
-def episodes_from_items(items: Items) -> Episodes:
-    episodes = []
-    for it in items:
-        enclosure = it.find("enclosure")
-        mp3_url = enclosure.attrs["url"]
-
-        image = it.find("itunes:image")
-        jpg_url = image.attrs["href"]
-
-        ep = Episode(mp3_url, jpg_url)
-        episodes.append(ep)
-
-    return episodes
+def get_episodes() -> dict:
+    url = URL_RSS
+    with requests.get(url, stream=True) as response:
+        parsed = podcastparser.parse(url, response.raw)
+    return parsed
 
 
 def download(url: str, file_path: str):
@@ -58,7 +24,8 @@ def download(url: str, file_path: str):
                 file.write(chunk)
 
 
-def download_episode(episode: Episode):
+def download_episode(episode: dict):
+    # todo switch from object to podcastparser dict
     mp3_url = episode.mp3_url
     mp3_path = "test.mp3"  # todo
     download(mp3_url, mp3_path)
@@ -70,4 +37,11 @@ def download_episode(episode: Episode):
     # todo fuse em with ffmpeg
 
 
-# todo main functions
+def main():
+    episodes = get_episodes()
+    # for ep in episodes:
+    # todo
+
+
+if __name__ == "__main__":
+    main()
