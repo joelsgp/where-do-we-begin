9bf6720285bc15621f13977349f3645420b46ad7
jmcb <joelsgp@protonmail.com>
2023-06-25 20:06:50 +0100

    Fix wget script

--- download-wget.sh
+++ download-wget.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eux
 
+# --timestamping skips downloads
+# if already downloaded the latest version.
+# see also --no-clobber
 wget \
     --show-progress \
+    --timestamping \
     --directory-prefix=downloads/ \
     --input-file=urls.txt
