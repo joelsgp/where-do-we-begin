0913ddfd8c0c11154623f3563e6eeaaf809a549d
jmcb <joelsgp@protonmail.com>
2023-06-25 22:38:48 +0100

    Add printing to separate.py

--- separate.py
+++ separate.py
@@ -46,17 +46,25 @@ def main():
         "Wayne",
     ]
     # load captions
+    print(f"loading captions from {sub_file}")
     captions = webvtt.from_sbv(sub_file)
+    print("loaded captions")
 
     # filter
+    print(f"filtering captions with {len(speakers)} speakers")
     caps_by_speaker = filter_captions_by_speaker(speakers, captions)
+    print("filtered captions")
 
     # write to label tracks
     DIRECTORY_LABELS.mkdir(exist_ok=True)
     for name, captions in caps_by_speaker.items():
+        print(f"{name} // {len(captions)}")
+        # get file to write to
         file_path = DIRECTORY_LABELS.joinpath(name).with_suffix(".txt")
         with open(file_path, "w") as file:
+            # convert to label track
             label_lines = captions_to_label_track(captions)
+            # write it
             file.writelines(label_lines)
 
 
