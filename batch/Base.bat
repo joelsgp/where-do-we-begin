397c945700cf3def30b34a41ce148063fc4e40fc
jmcb <joelsgp@protonmail.com>
2023-06-29 16:55:05 +0100

    Fix batch script

--- batch/Base.bat
+++ batch/Base.bat
@@ -1,2 +1,4 @@
-start wmplayer "C:\Documents and Settings\Joel\My Documents\My Music\1\Base.wma" /fullscreen
+start wmplayer "C:\Documents and Settings\Joel\My Documents\My Music\1\Base.wma"
+Rem 1s delay
+ping -n 2 127.0.0.1
 start wmplayer "C:\Documents and Settings\Joel\My Documents\My Music\1\Base.wma" /fullscreen
